cc7bb51ca50b4fccb6dda261d8f926a7333710f6
hmarui66 <hmarui66@gmail.com>
2026-01-19 23:34:46 +0900

    replace with Astro

--- content/pages/about.md
+++
@@ -1,7 +0,0 @@
----
-title: "About me"
-template: "page"
-socialImage: ""
----
-
-プログラミング(Golang, JS, etc..)、ジョギング、そして特に最近は数学を楽しんでいる。
---
+++ src/content/config.ts
@@ -0,0 +1,15 @@
+import { defineCollection, z } from 'astro:content';
+
+const postsCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    date: z.date(),
+    description: z.string().optional(),
+    tags: z.array(z.string()).optional(),
+  }),
+});
+
+export const collections = {
+  posts: postsCollection,
+};
